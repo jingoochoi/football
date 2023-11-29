2f22eec8db62e48b475c82eb3aec5c00144aef5a
jingoochoi <jingoo0206@gmail.com>
2023-11-29 16:27:58 +0900

    .

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   let play=Math.ceil(Math.random()*2)
   // console.log(play)
   const [voon,setVoon]=useState(0)
+  let moon
   // let voon=0
   const [goal,setGoal]=useState(0)
   // let goal=0
@@ -21,10 +22,10 @@ function App() {
       $('.ttxt').html(`
           ${ball[0][attk][0]}
           <br>
-          ${attk==0?'YOU':''} ${ball[0][attk][1]}
+          ${ball[0][attk][1]}
           <img src=${ball[0][attk][2]}>
       `)
-      let moon=voon+5
+      moon=voon+10
       setVoon(moon)
       if (attk==0) {
         let coal=goal+1
@@ -39,7 +40,7 @@ function App() {
           ${attk==0?'TEAM MATE':''} ${ball[0][attk][1]}
           <img src=${ball[0][attk][2]}>
       `)
-      let moon=voon+5
+      moon=voon+10
       setVoon(moon)
       if (attk==0) {
         let coal=goal+1
@@ -56,35 +57,44 @@ function App() {
       $('.ttxt').html(`
           ${ball[1][attk][0]}
           <br>
-          ${attk==0?'YOU':''} ${ball[1][attk][1]}
+          ${ball[1][attk][1]}
           <img src=${ball[1][attk][2]}>
       `)
-      let moon=voon+5
+      moon=voon+10
       setVoon(moon)
       if (attk==0) {
         let coal=juck+1
         setJuck(coal)
       }
     }
-    if (tg='PASS') {
+    if (tg='PRESS') {
       $('.comp').fadeIn(0).delay(3000).fadeOut(0)
       $('.ttxt').html(`
-          ${ball[0][attk][0]}
+          ${ball[1][attk][0]}
           <br>
-          ${attk==0?'TEAM MATE':''} ${ball[0][attk][1]}
-          <img src=${ball[0][attk][2]}>
+          ${ball[1][attk][1]}
+          <img src=${ball[1][attk][2]}>
       `)
-      let moon=voon+5
+      moon=voon+10
       setVoon(moon)
       if (attk==0) {
-        let coal=goal+1
-        setGoal(coal)
+        let coal=juck+1
+        setJuck(coal)
       }
     }
-    // console.log(voon)
+    // moon+=10
+  }
+  // console.log(voon)
+  if (voon==90) {
+    setTimeout(() => {
+      $('.stdu').html(`
+          <p>GAME SET.<br>MY TEAM ${goal} : ${juck} ENEMY.</p>
+      `).css({textAlign:'center'}).find('p').css({position:'absolute',top:'50%',left:'50%',transform:'translate(-50%,-50%)'})
+    }, 3000);
   }
   return (
     <>
+      <div className='jong'><div className='gtxt'></div></div>
       <div className='comp'><div className='ttxt'></div></div>
       <div className='stdu'>
         <p className='time'><span className='boon'>{voon}</span>min</p>
@@ -99,8 +109,8 @@ function App() {
         {play==2&&
         <>
         <img src={data.dfds.imgs} className='back'></img>
-        <div className='txt1'>{data.dfds.text[0]}</div>
-        <div className='txt2'>{data.dfds.text[1]}</div>
+        <div className='txt1' onClick={subi}>{data.dfds.text[0]}</div>
+        <div className='txt2' onClick={subi}>{data.dfds.text[1]}</div>
         </>
         }
       </div>
--- src/data/stat.js
+++ src/data/stat.js
@@ -2,5 +2,5 @@ export const ball=[
     // attack
     [['GOAL','SCORED','https://www.shutterstock.com/image-vector/soccer-ball-hitting-net-vector-260nw-2319839557.jpg'],['NO GOAL','KEEPER SAVED','https://png.pngtree.com/png-vector/20220715/ourmid/pngtree-sport-competition-stadium-goalkeeper-soccer-png-image_5926772.png']],
     // defend
-    [['FREEKICK GOAL','ENEMY SCORED','https://www.shutterstock.com/image-vector/soccer-ball-hitting-net-vector-260nw-2319839557.jpg'],['OUR KEEPER SAVED','IT IS OUR TURN','https://png.pngtree.com/png-vector/20220715/ourmid/pngtree-sport-competition-stadium-goalkeeper-soccer-png-image_5926772.png']],
+    [['FREEKICK GOAL','ENEMY SCORED','https://www.shutterstock.com/image-vector/soccer-ball-hitting-net-vector-260nw-2319839557.jpg'],['NICE DEFENCE','IT IS OUR TURN','https://thumbs.dreamstime.com/b/line-art-one-footballer-tackle-each-other-line-art-one-footballer-tackle-to-each-other-football-game-245472366.jpg']],
 ]
